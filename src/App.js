816cea1217b755e0db8493e7f3ef5221a9e9183f
https://github.com': codeplus254 <ronnymageh@gmail.com>
2019-08-27 10:58:34 +0300

    input references in react

--- src/App.js
+++ src/App.js
@@ -1,31 +1,94 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-const Temp = (props) => {
-  return (<div>{props.val}</div>)
+const StatelessNestedComponent = (props) => {
+  return (
+    <>
+    <input
+      ref={props.inputRef}
+      type="text" />
+    </>
+    
+  )
+}
+const FunctionalComponent = () => {
+  let inputRef = null;
+  const onClick = () => {
+    inputRef.focus();
+  }
+  return (
+    <>
+      <div>
+        <span>My Input</span>
+        <StatelessNestedComponent 
+         inputRef={(input) => {inputRef = input}}
+        />
+        <input 
+          type="submit" 
+          onClick={onClick} 
+          value="submit"/>
+      </div>
+
+    </>
+  )
 }
 
-class App extends PureComponent {
-  state = {
-    val: 1,
-  };
-
-  componentDidMount(){
-    setInterval(()=>{
-      this.setState(()=>{
-        return { val: 1}
-      })
-    }, 2000)
+class App extends Component {
+  onClick = () => {
+    alert(`yey : ${this.firstName.value} | ${this.lastName.value} submitted!!!`)
   }
-  // // prevent unnecessary re-renders when data is still the same
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   return ( this.state.val === nextState.val ? false : true ); 
-  // };
+
+  onKeyUp = (target, e) => {
+    if(e.keyCode === 13) {  // "ENTER" key on keyboard
+      this.lastName.focus();
+      switch(target) {
+        case 'firstName':
+          this.lastName.focus();
+          break;
+        case 'lastName':
+          this.age.focus();
+          break;
+        case 'age':
+          this.submit.focus();
+          break;
+        default:
+          this.firstName.focus();
+      }
+    }
+  }
 
   render() {
     return (
       <div className="App">
-        <Temp val={this.state.val}/>
+        <FunctionalComponent />
+        <div>
+          <span>First Name:</span>
+          <input 
+            ref={(input)=> {this.firstName = input}}
+            onKeyUp={this.onKeyUp.bind(this, 'firstName')}
+            type="text" />
+        </div>
+        <div>
+          <span>Last Name:</span>
+          <input 
+            ref={(input)=> {this.lastName = input}}
+            onKeyUp={this.onKeyUp.bind(this, 'lastName')}
+            type="text" />
+        </div>
+        <div>
+          <span>Age:</span>
+          <input 
+            ref={(input)=> {this.age = input}}
+            onKeyUp={this.onKeyUp.bind(this, 'age')}
+            type="text" />
+        </div>
+        <div>
+          <input 
+            ref={(input)=> {this.submit = input}}
+            onKeyUp={this.onKeyUp.bind(this, 'submit')}
+            type="submit" value="submit"
+            onClick = {this.onClick} />
+        </div>
       </div>
     );
   };
