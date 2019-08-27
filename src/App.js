ddc01f939b4d0ed39aa0dff8c65d7812758fba7a
https://github.com': codeplus254 <ronnymageh@gmail.com>
2019-08-27 12:04:13 +0300

    React Typechecking with props

--- src/App.js
+++ src/App.js
@@ -1,94 +1,62 @@
 import React, { Component } from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
 
-const StatelessNestedComponent = (props) => {
-  return (
+const Test = (props) => {
+  return(
     <>
-    <input
-      ref={props.inputRef}
-      type="text" />
-    </>
-    
-  )
-}
-const FunctionalComponent = () => {
-  let inputRef = null;
-  const onClick = () => {
-    inputRef.focus();
-  }
-  return (
-    <>
+      <h1>{props.str}</h1>
+      <h1>{(props.bool? 'passed in a bool' : 'no bool provided')}</h1>
+      <h1>{props.stringOrNumber}</h1>
       <div>
-        <span>My Input</span>
-        <StatelessNestedComponent 
-         inputRef={(input) => {inputRef = input}}
-        />
-        <input 
-          type="submit" 
-          onClick={onClick} 
-          value="submit"/>
+        {
+          props.numberArray.map((val)=> {
+            return(<li key={val}>{val}</li>);
+          })
+        }
       </div>
-
+      <div>
+        {
+          props.objectArray.map((val)=> {
+            return(<li key={val.age}>{val.name}</li>);
+          })
+        }
+      </div>
+      <div><h1>{props.children}</h1></div>
     </>
-  )
+  );
+}
+
+Test.propTypes = {
+  str: PropTypes.string,
+  bool: PropTypes.bool,
+  stringOrNumber: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  numberArray: PropTypes.arrayOf(PropTypes.number),
+  objectArray:PropTypes.arrayOf(PropTypes.shape(
+    {
+      name: PropTypes.string,
+      age: PropTypes.number
+    }
+  )),
+  children: PropTypes.element.isRequired
 }
 
 class App extends Component {
-  onClick = () => {
-    alert(`yey : ${this.firstName.value} | ${this.lastName.value} submitted!!!`)
-  }
-
-  onKeyUp = (target, e) => {
-    if(e.keyCode === 13) {  // "ENTER" key on keyboard
-      this.lastName.focus();
-      switch(target) {
-        case 'firstName':
-          this.lastName.focus();
-          break;
-        case 'lastName':
-          this.age.focus();
-          break;
-        case 'age':
-          this.submit.focus();
-          break;
-        default:
-          this.firstName.focus();
-      }
-    }
-  }
-
   render() {
     return (
       <div className="App">
-        <FunctionalComponent />
-        <div>
-          <span>First Name:</span>
-          <input 
-            ref={(input)=> {this.firstName = input}}
-            onKeyUp={this.onKeyUp.bind(this, 'firstName')}
-            type="text" />
-        </div>
-        <div>
-          <span>Last Name:</span>
-          <input 
-            ref={(input)=> {this.lastName = input}}
-            onKeyUp={this.onKeyUp.bind(this, 'lastName')}
-            type="text" />
-        </div>
-        <div>
-          <span>Age:</span>
-          <input 
-            ref={(input)=> {this.age = input}}
-            onKeyUp={this.onKeyUp.bind(this, 'age')}
-            type="text" />
-        </div>
-        <div>
-          <input 
-            ref={(input)=> {this.submit = input}}
-            onKeyUp={this.onKeyUp.bind(this, 'submit')}
-            type="submit" value="submit"
-            onClick = {this.onClick} />
-        </div>
+        <Test 
+          str={'Yay'}
+          bool
+          stringOrNumber={10}
+          numberArray={[1,6,9]}
+          objectArray={[{name:'john', age:10}, {name: 'Peter', age:11}]}
+          >
+          <div>
+            Child
+          </div>
+        </Test>
+          
       </div>
     );
   };
